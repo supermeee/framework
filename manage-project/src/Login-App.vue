db4ff5a10e8b62106e030d8b2c5972d58b7557ba
wudizhangzhi <wudizhangzhi@163.com>
2018-04-08 21:34:43 +0800

    尝试多页面模式

---
+++ manage-project/src/Login-App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="login-app">
+    <div class="content">
+      <p>this is a login view</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login-App',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+} */
+.content{
+  position: absolute;
+  top: 51px;
+  left: 241px;
+  right: 0;
+  bottom: 0;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+</style>
